81f27891256f85025d175943b42eb8167bc90e1b
Santiago Devesa <sdevesa@itba.edu.ar>
2025-07-13 22:11:20 -0300

    add: security

--- src/management/management_command.c
+++ src/management/management_command.c
@@ -6,14 +6,14 @@
 
 static bool management_process_command(struct management_command_parser *parser, struct buffer * buffer);
 
-typedef void (*management_command_handler)(struct management_command_parser *parser, struct buffer *response_buffer);
+typedef bool (*management_command_handler)(struct management_command_parser *parser, struct buffer *response_buffer);
 
-static void users_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
-static void add_user_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
-static void delete_users_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
-static void change_password_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
-static void stats_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
-static void change_role_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool users_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool add_user_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool delete_users_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool change_password_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool stats_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
+static bool change_role_command_handler(struct management_command_parser *parser, struct buffer *response_buffer);
 
 static management_command_handler command_handlers[] = {
     users_command_handler,
@@ -44,11 +44,19 @@ int management_command_read(struct selector_key *key) {
 
     buffer_write_adv(&data->request_buffer, read_count);
     management_command_parser_parse(&data->management_parser.request_parser, &data->request_buffer);
-    if(management_command_parser_is_done(&data->management_parser.request_parser)) {
-        if (selector_set_interest_key(key, OP_WRITE) != SELECTOR_SUCCESS ||
-        !management_process_command(&data->management_parser.request_parser, &data->response_buffer)) {
+
+    if (management_command_parser_is_done(&data->management_parser.request_parser)) {
+        if (management_command_parser_has_error(&data->management_parser.request_parser)) {
             return MANAGEMENT_ERROR;
         }
+
+        if (!management_process_command(&data->management_parser.request_parser,
+                                        &data->response_buffer)) {
+            return MANAGEMENT_ERROR;
+        }
+        if (selector_set_interest_key(key, OP_WRITE) != SELECTOR_SUCCESS)
+            return MANAGEMENT_ERROR;
+
         data->command = data->management_parser.request_parser.command;
         return MANAGEMENT_REQUEST_WRITE;
     }
@@ -77,69 +85,65 @@ int management_command_write(struct selector_key *key) {
     return MANAGEMENT_CLOSED;
 }
 
-static void add_user_command_handler(struct management_command_parser *parser,
+static bool add_user_command_handler(struct management_command_parser *parser,
                                      struct buffer *response_buffer) {
 
     if (parser->read_args != 2) {
-        management_command_parser_build_response(parser, response_buffer,
-                                                 MANAGEMENT_INVALID_ARGUMENTS,
-                                                 "add_user: invalid argument count, expected 2");
-        return;
+        return management_command_parser_build_response(parser, response_buffer,
+                                                        MANAGEMENT_INVALID_ARGUMENTS,
+                                                        "add_user: invalid argument count, expected 2");;
     }
 
     const char *username = (const char *)parser->args[0];
     const char *password = (const char *)parser->args[1];
 
     if (exists_user(username)) {
-        management_command_parser_build_response(parser, response_buffer,
+        return management_command_parser_build_response(parser, response_buffer,
                                                  MANAGEMENT_USER_ALREADY_EXISTS,
                                                  "add_user: user already exists");
-        return;
     }
 
     if (create_user(username, password, false)) {
-        management_command_parser_build_response(parser, response_buffer,
+        return management_command_parser_build_response(parser, response_buffer,
                                                  MANAGEMENT_SUCCESS,
                                                  "add_user: user added successfully");
-        return;
     }
 
-    management_command_parser_build_response(parser, response_buffer,
+    return management_command_parser_build_response(parser, response_buffer,
                                              MANAGEMENT_SERVER_ERROR,
                                              "add_user: failed to add user");
 }
 
-static void delete_users_command_handler(struct management_command_parser *parser,
+static bool delete_users_command_handler(struct management_command_parser *parser,
                                          struct buffer *response_buffer) {
     if (parser->read_args != 1) {
-        management_command_parser_build_response(parser, response_buffer,
+        return management_command_parser_build_response(parser, response_buffer,
                                                  MANAGEMENT_INVALID_ARGUMENTS,
                                                  "delete_users: invalid argument count, expected 1");
-        return;
     }
 
     const char *username = (const char *)parser->args[0];
     if (!exists_user(username)) {
-        management_command_parser_build_response(parser, response_buffer,
+        return management_command_parser_build_response(parser, response_buffer,
                                                  MANAGEMENT_USER_NOT_FOUND,
                                                  "delete_users: user not found");
-        return;
     }
     if (delete_user(username)) {
-        management_command_parser_build_response(parser, response_buffer,
+        return management_command_parser_build_response(parser, response_buffer,
                                                  MANAGEMENT_SUCCESS,
                                                  "delete_users: user deleted successfully");
-        return;
     }
-    management_command_parser_build_response(parser, response_buffer,
+    return management_command_parser_build_response(parser, response_buffer,
                                              MANAGEMENT_SERVER_ERROR,
                                              "delete_users: failed to delete user");
 }
 
-static bool management_process_command(struct management_command_parser *parser, struct buffer * buffer) {
-    if(parser->command <= MANAGEMENT_COMMAND_MAX) {
-        command_handlers[parser->command](parser, buffer);
-        return true;
-    }
-    return false;
+static bool management_process_command(struct management_command_parser *parser,
+                                       struct buffer *resp)
+{
+    size_t n = sizeof command_handlers / sizeof command_handlers[0];
+    if ((size_t)parser->command >= n)          /* enum fuera de rango */
+        return false;
+
+    return command_handlers[parser->command](parser, resp);
 }
